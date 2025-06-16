39eda5f9e788f3ed5f43625bce5446fbe1727fc8
Martin Matejovsky <matejovsky.ma@volny.cz>
2025-06-16 17:14:39 +0200

    setting initialized state of game

--- src/components/GridTile.tsx
+++ src/components/GridTile.tsx
@@ -1,35 +1,13 @@
 import "../styles/GridTiles.scss";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { GameGrid } from "../utils/types";
 
 type Props = {
   setGameState: Dispatch<SetStateAction<boolean>>;
 };
-type GameGrid = Boolean[][];
+const gridSize = 5;
 
 export default function GridTile({ setGameState }: Props) {
-  const gridSize = 5;
-  const emptyGrid: GameGrid = Array.from({ length: gridSize }, () =>
-    Array(gridSize).fill(true),
-  );
-  const [gridPrepared, setGridPrepared] = useState(false);
-
-  // STATE
-  const [gameGrid, setGameGrid] = useState(emptyGrid);
-
-  // EFFECTS
-  useEffect(() => {
-    if (!gridPrepared) return;
-    setGameState(evaluateWinningCondition());
-  }, [gameGrid]);
-
-  // shuffle grid randomly
-  useEffect(() => {
-    shuffleGrid();
-    // TODO
-    if (evaluateWinningCondition()) console.log("should shuffle one more time");
-    setGridPrepared(true);
-  }, []);
-
   // HANDLERS
   const handleTileClick = (
     grid: GameGrid,
@@ -47,27 +25,13 @@ export default function GridTile({ setGameState }: Props) {
     toggle(rowIndex, columnIndex - 1);
     toggle(rowIndex, columnIndex + 1);
 
-    console.log("set grid");
     return grid;
   };
 
   // METHODS
-  const shuffleGrid = () => {
-    const gridCopy = structuredClone(emptyGrid);
-
-    for (let i = 0; i < gridSize; i++) {
-      const randomRow = Math.floor(Math.random() * gridSize);
-      const randomColumn = Math.floor(Math.random() * gridSize);
-
-      handleTileClick(gridCopy, randomRow, randomColumn);
-    }
-
-    setGameGrid(gridCopy);
-  };
-
-  const evaluateWinningCondition = () => {
-    const firstTileState: Boolean = gameGrid[0][0];
-    for (const row of gameGrid) {
+  const evaluateWinningCondition = (evaluatedGrid: GameGrid) => {
+    const firstTileState: Boolean = evaluatedGrid[0][0];
+    for (const row of evaluatedGrid) {
       if (row.findIndex((cell) => cell !== firstTileState) >= 0) {
         return false;
       }
@@ -75,6 +39,33 @@ export default function GridTile({ setGameState }: Props) {
 
     return true;
   };
+
+  // This serves as a solution to the fact that not all random color setups are solvable
+  const shuffleGrid = () => {
+    const emptyGrid: GameGrid = Array.from({ length: gridSize }, () =>
+      Array(gridSize).fill(true),
+    );
+    let newGrid = structuredClone(emptyGrid);
+
+    do {
+      for (let i = 0; i < gridSize; i++) {
+        const randomRow = Math.floor(Math.random() * gridSize);
+        const randomColumn = Math.floor(Math.random() * gridSize);
+
+        newGrid = handleTileClick(newGrid, randomRow, randomColumn);
+      }
+    } while (evaluateWinningCondition(newGrid));
+
+    return newGrid;
+  };
+
+  // STATE
+  const [gameGrid, setGameGrid] = useState<GameGrid>(shuffleGrid());
+
+  // EFFECTS
+  useEffect(() => {
+    setGameState(evaluateWinningCondition(gameGrid));
+  }, [setGameState, gameGrid]);
 
   return (
     <div className="grid-tile">
