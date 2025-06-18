81607d4928dc387d6c071bc1abed0f5b527da375
Martin Matejovsky <matejovsky.ma@volny.cz>
2025-06-18 12:38:14 +0200

    favicon

--- src/components/MotivationLetter.tsx
+++ src/components/MotivationLetter.tsx
@@ -16,17 +16,18 @@ export default function MotivationLetter() {
       </p>
       <p>
         Vidím na Vašich stránkách volnou pozici backend developera, a to bohužel
-        není moje parketa (i když JavaScript a TypeScript používám). Rád bych se
-        Vám i tak uvedl ve známost, mým malým koníčkem je totiž tvorba her, a
-        třeba právě tento aspekt - nápady, kreativitu a hlavně zápal - bych mohl
-        do Vaší party přinést a cítit naplnění, že konečně je moje hobby mou
-        prací. Projekty jako Ylands by nejvíce ladily s mou krevní skupinou.
-        Hádám, že lidí, kteří by chtěli vytvářet nápady pro hry, se vám hlásí
-        hromada, takže snad by moje technické znalosti z vývoje webů byly pro
-        Bohemia Interactive přínosnější, a třeba se budu moct přidávat do kvasu
-        myšlenek a chodbových smalltalků a tam svou nápaditost ventilovat
-        (upřímnou soustrast). Baví mě vymýšlet zápletky, mikro scénky, mini
-        úkoly, úkolové principy.
+        není moje parketa (i když JavaScript a TypeScript používám a nemám
+        problém se rozvíjet na backend vývojáře). Rád bych se Vám i tak uvedl ve
+        známost, mým malým koníčkem je totiž tvorba her, a třeba právě tento
+        aspekt - nápady, kreativitu a hlavně zápal - bych mohl do Vaší party
+        přinést a cítit naplnění, že konečně je moje hobby mou prací. Projekty
+        jako Ylands by nejvíce ladily s mou krevní skupinou. Hádám, že lidí,
+        kteří by chtěli vytvářet nápady pro hry, se vám hlásí hromada, takže
+        snad by moje technické znalosti z vývoje webů byly pro Bohemia
+        Interactive přínosnější, a třeba se budu moct přidávat do kvasu myšlenek
+        a chodbových smalltalků a tam svou nápaditost ventilovat (upřímnou
+        soustrast). Baví mě vymýšlet zápletky, mikro scénky, mini úkoly, úkolové
+        principy.
       </p>
       <p>
         Budu nadšený, pokud se rozhodnete mě kontaktovat a zjistíme, v čem bych
